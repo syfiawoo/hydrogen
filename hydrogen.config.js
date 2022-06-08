b1322c58667ffaec97b63bcecc4bdde89f5007a7
Seth Fiawoo <seth.fiawoo@shopify.com>
2022-06-08 16:08:17 -0600

    initial commit

---
+++ hydrogen.config.js
@@ -0,0 +1,9 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    storeDomain: 'fiawoos.myshopify.com',
+    storefrontToken: 'ae43a1340aa70b735dda9799aebb7758',
+    storefrontApiVersion: '2022-07',
+  },
+});
---
+++ src/routes/Index.server.jsx
@@ -0,0 +1,19 @@
+import { useShopQuery } from "@shopify/hydrogen";
+import { gql } from "@shopify/hydrogen";
+
+export default function Home() {
+  const {data} = useShopQuery({
+    query: QUERY
+  })
+  console.log(data)
+  return <h1 className="text-3xl font-bold">Hello {data.shop.name}</h1>;
+}
+
+const QUERY = gql`
+ query shopName {
+   shop {
+     name
+     description
+   }
+ }
+`
